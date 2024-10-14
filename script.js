30dd5ef73d1c841227994ffc4ab4685d344fc955
hi570stars <71354208+hi570stars@users.noreply.github.com>
2024-10-14 11:41:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+
+// Function to change the text when button is clicked
+function changeText() {
+  document.getElementById("myParagraph").innerText = "You clicked the button!";
+}
+
+function goToTetris(){
+  window.open("https://github.com/cs314h-fa24/314h-prog4-prog4group4", "_blank");
+}
+function goToCritters(){
+  window.open("https://github.com/cs314h-fa24/314h-prog3-prog3group9", "_blank");
+}
+
+function goToToolkit(){
+  window.open("https://github.com/chomarieh2006/Blossom-App", "_blank");
+}
+function goToApp(){
+  window.open("https://github.com/hi570stars/TaxXpertsAppDevA", "_blank");
+}
